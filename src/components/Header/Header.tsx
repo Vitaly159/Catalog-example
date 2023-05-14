4a39f627dadeb0e066bc534388842e9c6271051f
vital <159vvf@gmail.com>
2023-05-14 23:13:39 +0500

    зафиксировал хедер

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,20 @@
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { useNavigate } from "react-router";
 import { setChosenProduct } from "../../reducer/reducer";
+import { CartProduct } from "../../reducer/reducer";
 import styles from "./Header.module.css";
 
 const Header = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const cartProducts = useAppSelector((state) => state.catalog.cartProducts);
+  const [amountProductInCart, setAmountProductInCart] = useState<number>()
+
+  useEffect(() => {
+    const amount = JSON.parse(localStorage.getItem('myProducts')!).length
+    setAmountProductInCart(amount)
+  }, [cartProducts])
 
   const goToCart = (): void => {
     dispatch(setChosenProduct(null));
@@ -15,7 +24,7 @@ const Header = () => {
   return (
     <div className={styles.wrapper}>
       <button className={styles.shoppingCart} onClick={goToCart}>
-        Корзина
+        {`Корзина: ${amountProductInCart} товаров`}
       </button>
     </div>
   );
